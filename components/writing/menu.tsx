afb86011ded176545d73e9e713f92c4c52e92dde
yunger <hello@yunger.dev>
2025-02-06 15:18:04 -0300

    fix: fix line not visible in light mode

--- components/writing/menu.tsx
+++ components/writing/menu.tsx
@@ -17,7 +17,7 @@ export function Menu({ posts }: MenuProps) {
                     action={() => console.log(post.title)}
                 >
                     <h2 className="text-md text-left">{post.title}</h2>
-                    <div className="h-[1px] min-w-12 flex-grow border-b border-border" />
+                    <div className="h-[1px] min-w-12 flex-grow border-b border-input" />
                     <div className="whitespace-nowrap">{post.date}</div>
                 </MenuItem>
             ))}
