281f4d79aac9a83967b34ed7b8c8436579eaaf38
yunger <hello@yunger.dev>
2025-02-11 16:37:38 -0300

    refactor: add max length limit to note

--- app/about/forms.ts
+++ app/about/forms.ts
@@ -1,5 +1,8 @@
 import { z } from "zod";
 
 export const noteSchema = z.object({
-    content: z.string().min(1, { message: "Note cannot be empty" }),
+    content: z
+        .string()
+        .min(1, { message: "Note cannot be empty" })
+        .max(2000, { message: "Note cannot be longer than 2000 characters" }),
 });
