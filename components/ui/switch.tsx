2a903008b74cb963677e467fe47baba42db4faff
yunger <hello@yunger.dev>
2025-02-21 11:30:27 -0300

    feat: add custom `Switch` component

---
+++ components/ui/switch.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import * as React from "react";
+import * as SwitchPrimitives from "@radix-ui/react-switch";
+
+import { Label } from "@/components/ui/label";
+
+import { cn } from "@/lib/utils";
+
+type SwitchProps = React.ComponentPropsWithoutRef<
+    typeof SwitchPrimitives.Root
+> & {
+    id: string;
+    label: string;
+};
+
+const Switch = React.forwardRef<
+    React.ElementRef<typeof SwitchPrimitives.Root>,
+    SwitchProps
+>(({ className, id, label, ...props }, ref) => (
+    <div className="flex w-full items-center justify-between gap-2">
+        <Label htmlFor={id} className="w-full">
+            {label}
+        </Label>
+        <SwitchPrimitives.Root
+            id={id}
+            className={cn(
+                "peer inline-flex cursor-pointer items-center justify-between focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50",
+                className,
+            )}
+            {...props}
+            ref={ref}
+        >
+            <span
+                className={cn("mr-2 font-medium", {
+                    "text-primary underline": !props.checked,
+                })}
+            >
+                No
+            </span>
+            /
+            <span
+                className={cn("ml-2 font-medium", {
+                    "text-primary underline": props.checked,
+                })}
+            >
+                Yes
+            </span>
+        </SwitchPrimitives.Root>
+    </div>
+));
+Switch.displayName = SwitchPrimitives.Root.displayName;
+
+export { Switch };
