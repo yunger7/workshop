7ee4288459432d4aec084e5885630de2105fe741
yunger <hello@yunger.dev>
2025-03-09 20:10:11 -0300

    fix: fix help dialog responsiveness in mobile

--- components/help-dialog.tsx
+++ components/help-dialog.tsx
@@ -25,7 +25,7 @@ type HelpSectionProps = React.ComponentPropsWithoutRef<"div"> & {
 
 function HelpSection({ title, commands, ...props }: HelpSectionProps) {
     return (
-        <div {...props}>
+        <div className="mb-8" {...props}>
             {title && <p className="mb-2">{title}:</p>}
             <ul className={cn({ "pl-4": title })}>
                 {commands.map(({ keys, description, separator }, index) => (
@@ -50,78 +50,93 @@ export function HelpDialog() {
         <Dialog open={isHelpDialogOpen} onOpenChange={setIsHelpDialogOpen}>
             <DialogContent>
                 <DialogTitle>Help</DialogTitle>
-                <div className="grid grid-cols-2 place-content-between gap-8 p-8">
-                    <HelpSection
-                        title="Global shortcuts"
-                        commands={[
-                            { keys: ["space"], description: "Open menu" },
-                            {
-                                keys: ["mod", "h"],
-                                description: "Toggle help dialog",
-                            },
-                            {
-                                keys: ["mod", "k"],
-                                description: "Toggle search dialog",
-                            },
-                            { keys: ["mod", "j"], description: "Toggle theme" },
-                        ]}
-                    />
-                    <div className="ml-auto flex flex-col items-center gap-4">
-                        <ul className="relative size-[100px]">
-                            <li className="absolute left-0 top-1/2 flex -translate-y-1/2 items-center gap-2">
-                                <IconArrowLeft className="size-4" />h
-                            </li>
-                            <li className="absolute right-0 top-1/2 flex -translate-y-1/2 items-center gap-2">
-                                l
-                                <IconArrowRight className="size-4" />
-                            </li>
-                            <li className="absolute left-1/2 top-0 flex -translate-x-1/2 flex-col items-center gap-2">
-                                <IconArrowUp className="size-4" />k
-                            </li>
-                            <li className="absolute bottom-0 left-1/2 flex -translate-x-1/2 flex-col items-center gap-2">
-                                j
-                                <IconArrowDown className="size-4" />
-                            </li>
-                        </ul>
-                        <p>(Arrow keys also work)</p>
+                <div className="flex max-h-[80vh] flex-col justify-between gap-x-8 overflow-y-auto p-8 sm:flex-row">
+                    <div className="order-last sm:order-first">
+                        <HelpSection
+                            title="Global shortcuts"
+                            commands={[
+                                { keys: ["space"], description: "Open menu" },
+                                {
+                                    keys: ["mod", "h"],
+                                    description: "Toggle help dialog",
+                                },
+                                {
+                                    keys: ["mod", "k"],
+                                    description: "Toggle search dialog",
+                                },
+                                {
+                                    keys: ["mod", "j"],
+                                    description: "Toggle theme",
+                                },
+                            ]}
+                        />
+                        <HelpSection
+                            title="Vim commands"
+                            commands={[
+                                {
+                                    keys: [":"],
+                                    description: "Enter command mode",
+                                },
+                                {
+                                    keys: [":!"],
+                                    description: "Execute shell command",
+                                },
+                                { keys: ["/"], description: "Search forward" },
+                                { keys: ["?"], description: "Search backward" },
+                                {
+                                    keys: ["n"],
+                                    description: "Move to next match",
+                                },
+                                {
+                                    keys: ["N"],
+                                    description: "Move to previous match",
+                                },
+                                {
+                                    keys: ["gg"],
+                                    description: "Move to top of file",
+                                },
+                                {
+                                    keys: ["G"],
+                                    description: "Move to bottom of file",
+                                },
+                            ]}
+                        />
                     </div>
-                    <HelpSection
-                        title="Vim commands"
-                        commands={[
-                            { keys: [":"], description: "Enter command mode" },
-                            {
-                                keys: [":!"],
-                                description: "Execute shell command",
-                            },
-                            { keys: ["/"], description: "Search forward" },
-                            { keys: ["?"], description: "Search backward" },
-                            { keys: ["n"], description: "Move to next match" },
-                            {
-                                keys: ["N"],
-                                description: "Move to previous match",
-                            },
-                            {
-                                keys: ["gg"],
-                                description: "Move to top of file",
-                            },
-                            {
-                                keys: ["G"],
-                                description: "Move to bottom of file",
-                            },
-                        ]}
-                    />
-                    <HelpSection
-                        className="ml-auto"
-                        title="Navigation"
-                        commands={[
-                            {
-                                keys: ["esc", "backspace"],
-                                separator: "/",
-                                description: "Go back",
-                            },
-                            { keys: ["mod", "enter"], description: "Execute" },
-                        ]}
-                    />
+                    <div className="order-first sm:order-last">
+                        <div className="mb-8 flex flex-col items-start gap-4 sm:items-center">
+                            <ul className="relative size-[100px]">
+                                <li className="absolute left-0 top-1/2 flex -translate-y-1/2 items-center gap-2">
+                                    <IconArrowLeft className="size-4" />h
+                                </li>
+                                <li className="absolute right-0 top-1/2 flex -translate-y-1/2 items-center gap-2">
+                                    l
+                                    <IconArrowRight className="size-4" />
+                                </li>
+                                <li className="absolute left-1/2 top-0 flex -translate-x-1/2 flex-col items-center gap-2">
+                                    <IconArrowUp className="size-4" />k
+                                </li>
+                                <li className="absolute bottom-0 left-1/2 flex -translate-x-1/2 flex-col items-center gap-2">
+                                    j
+                                    <IconArrowDown className="size-4" />
+                                </li>
+                            </ul>
+                            <p>(Arrow keys also work)</p>
+                        </div>
+                        <HelpSection
+                            title="Navigation"
+                            commands={[
+                                {
+                                    keys: ["esc", "backspace"],
+                                    separator: "/",
+                                    description: "Go back",
+                                },
+                                {
+                                    keys: ["mod", "enter"],
+                                    description: "Execute",
+                                },
+                            ]}
+                        />
+                    </div>
                 </div>
             </DialogContent>
         </Dialog>
