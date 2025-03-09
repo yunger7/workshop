dcfdd7b9bf14a7901fa7017ab9072da25f7229ff
yunger <hello@yunger.dev>
2025-03-09 19:37:32 -0300

    fix: fix grid background height limit when page has scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,7 +71,10 @@ export default async function RootLayout({
             <body
                 className={`${jetBrainsMono.variable} font-mono text-sm antialiased selection:bg-[#81a1c19f] selection:text-nord-polar-1 dark:selection:bg-[#88c0d080] dark:selection:text-nord-snow-3`}
             >
-                <div className="absolute inset-0 -z-10 h-full w-full bg-background bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]" />
+                <div
+                    id="grid-background"
+                    className="fixed inset-0 -z-10 h-full w-full bg-background bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"
+                />
                 <ThemeProvider
                     disableTransitionOnChange
                     enableSystem={false}
