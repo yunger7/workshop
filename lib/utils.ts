cdac9adc8aa4a22e8f4fcc55afd3618729c5cc05
yunger <hello@yunger.dev>
2025-03-10 10:39:47 -0300

    feat: add utility function to get site url

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,6 +5,18 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
+export function getURL() {
+    let url =
+        process?.env?.NEXT_PUBLIC_SITE_URL ??
+        process?.env?.NEXT_PUBLIC_VERCEL_URL ??
+        "https://yunger.dev";
+
+    url = url.startsWith("http") ? url : `https://${url}`;
+    url = url.endsWith("/") ? url.slice(0, -1) : url;
+
+    return url;
+}
+
 export function removeTrailingSlash(str: string): string {
     return str.replace(/\/$/, "");
 }
