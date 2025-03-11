0018db8608929d0656385c19503bffabb241a4e6
yunger <hello@yunger.dev>
2025-03-11 14:59:48 -0300

    build: fix build errors

--- src/lib/getPaperCoverImage.js
+++ src/lib/getPaperCoverImage.js
@@ -1,18 +1,32 @@
 import { getPlaiceholder } from "plaiceholder";
 
+const getImage = async (src) => {
+    const buffer = await fetch(src).then(async (res) =>
+        Buffer.from(await res.arrayBuffer())
+    );
+
+    const {
+        metadata: { height, width },
+        ...plaiceholder
+    } = await getPlaiceholder(buffer, { size: 10 });
+
+    return {
+        ...plaiceholder,
+        img: { src, height, width },
+    };
+};
+
 export async function getPaperCoverImage(page) {
-	const image = page.cover;
+    const image = page.cover;
 
-	if (image && image.type === "external") {
-		const { base64, img: imageData } = await getPlaiceholder(
-			image.external.url
-		);
+    if (image && image.type === "external") {
+        const { base64, img: imageData } = await getImage(image.external.url);
 
-		return {
-			...imageData,
-			blurDataURL: base64,
-		};
-	}
+        return {
+            ...imageData,
+            blurDataURL: base64,
+        };
+    }
 
-	return null;
+    return null;
 }
