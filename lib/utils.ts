f1d7da46f6911b699adf1d7edc4f0d747a00a816
yunger <hello@yunger.dev>
2025-03-11 14:37:37 -0300

    refactor: compute `fallbackUrl` using environment

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,15 +1,23 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { getEnvironment, Environment } from "@/lib/environment";
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
 export function getURL() {
+    const environment = getEnvironment();
+
+    const fallbackUrl =
+        environment === Environment.Dev
+            ? `http://localhost:${process.env.PORT || 3000}`
+            : "https://yunger.dev";
+
     let url =
         process?.env?.NEXT_PUBLIC_SITE_URL ??
         process?.env?.NEXT_PUBLIC_VERCEL_URL ??
-        "https://yunger.dev";
+        fallbackUrl;
 
     url = url.startsWith("http") ? url : `https://${url}`;
     url = url.endsWith("/") ? url.slice(0, -1) : url;
